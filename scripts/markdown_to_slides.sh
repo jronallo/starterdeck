b698d2d4e596cc9fe769d221166d2527fc26d161
Jason Ronallo <jronallo@gmail.com>
2014-03-22 18:45:19 -0400

    include section divs in fullpage output

--- scripts/markdown_to_slides.sh
+++ scripts/markdown_to_slides.sh
@@ -10,4 +10,4 @@ pandoc -w dzslides --include-after-body=templates/include-after-body-slides.html
 # other text. Sometimes this is necessary when a script can't be turned into a data URI by pandoc.
 # sed 's/<!-- insert before include-after-body-slides -->/<script src="\/socket.io\/socket.io.js" type="text\/javascript"><\/script><script src="http:\/\/localhost:35729\/livereload.js"><\/script>/' presentation-temp.html > presentation.html
 
-pandoc --include-after-body=templates/include-after-body-fullpage.html --standalone --self-contained presentation.md > presentation-fullpage.html
+pandoc --include-after-body=templates/include-after-body-fullpage.html --standalone --self-contained --section-divs presentation.md > presentation-fullpage.html
