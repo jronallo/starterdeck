2d6fe799a398687fd9e78ff305f3fc1751586a86
Jason Ronallo <jronallo@gmail.com>
2014-03-22 18:16:28 -0400

    some stuff really does need to just be inserted instead of having pandoc do it and try to create data URIs out of it. for instance socket.io really does not make sense being added by pandoc.

--- scripts/markdown_to_slides.sh
+++ scripts/markdown_to_slides.sh
@@ -6,4 +6,8 @@
 
 pandoc -w dzslides --include-after-body=templates/include-after-body-slides.html --standalone --self-contained presentation.md > presentation.html
 
+# The line above could be changed to output to presentation-tmp.html and this line uncommented to use sed to insert some
+# other text. Sometimes this is necessary when a script can't be turned into a data URI by pandoc.
+# sed 's/<!-- insert before include-after-body-slides -->/<script src="\/socket.io\/socket.io.js" type="text\/javascript"><\/script><script src="http:\/\/localhost:35729\/livereload.js"><\/script>/' presentation-temp.html > presentation.html
+
 pandoc --include-after-body=templates/include-after-body-fullpage.html --standalone --self-contained presentation.md > presentation-fullpage.html
